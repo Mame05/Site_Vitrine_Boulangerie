0afb1b3f00de22b852cd1204d230ed7203a4032a
Maréme Thiaw <mariamathiaw92@gmail.com>
2025-05-21 17:20:46 +0100

    Perfectionnement du menu burguer pour que lorsque l'on click sur le lien le menu se referme automatiquement

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggler = document.querySelector(".navbar-toggler");
   const menuIcon = toggler.querySelector(".menu-icon");
   const closeIcon = toggler.querySelector(".close-icon");
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link"); // Sélectionne tous les liens
+
 
   toggler.addEventListener("click", function () {
     // Bootstrap ajoute ou enlève la classe 'collapsed'
@@ -17,6 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
       menuIcon.classList.add("d-none");
       closeIcon.classList.remove("d-none");
     }
+  });
+  // Fermer le menu après clic sur un lien
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      if (window.innerWidth < 992 && !toggler.classList.contains("collapsed")) {
+        toggler.click(); // Simule un clic pour fermer le menu
+      }
+    });
   });
 });
 
